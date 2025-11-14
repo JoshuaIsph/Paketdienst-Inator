4c7d6b4ec5e9d56bb31b795fbfe71ef792feae4a
Michael Breitinger <breitingermichael@gmail.com>
2025-11-14 16:49:28 +0100

    Added sensor selftest

--- src/HardwareControl.h
+++ src/HardwareControl.h
@@ -4,19 +4,57 @@
 #include "Utils.h"
 #include "Peripheral.h"
 #include "Settings.h"
+#include "logger.h"
 
 
 // TODO: speed adjustment on the hill
 
+// Test for hardware failure on startup
+void hardwareControl_internal_selfTest() {
+
+  byte errorFlag = 0;
+
+  if(SensorValue(LEFT_SENSOR) == 0) {
+    errorFlag = 1;
+    log_println("Left Light Sensor");
+  }
+  if(SensorValue(RIGHT_SENSOR) == 0) {
+    errorFlag = 1;
+    log_println("Right Light Sensor");
+  }
+  if(SensorValue(MIDDLE_SENSOR) == 0) {
+    errorFlag = 1;
+    log_println("Middle Light Sensor");
+  }
+  if(SensorUS(ULTRA_SONIC_SENSOR) == 0) {
+    errorFlag = 1;
+    log_println("Ultra Sonic Sensor");
+  }
+
+
+  if(errorFlag) {
+
+    log_println("Not responding");
+    log_playAlarm();
+    log_waitForUserInput();
+  }
+
+  log_playStatusSound();
+}
+
 
 // configure sensors
 void hardwareControl_init() {
-  SetSensorType(LEFT_SENSOR, SENSOR_TYPE_LIGHT);
-  SetSensorType(RIGHT_SENSOR, SENSOR_TYPE_LIGHT);
-  SetSensorType(BARCODE_SENSOR, SENSOR_TYPE_LIGHT);
+  
   SetSensorLight(LEFT_SENSOR, true);
   SetSensorLight(RIGHT_SENSOR, true);
   SetSensorLight(BARCODE_SENSOR, true);
+
+  SetSensorLowspeed(ULTRA_SONIC_SENSOR);
+
+
+  hardwareControl_internal_selfTest();
+
 }
 
 
