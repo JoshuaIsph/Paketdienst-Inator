6846f99d2bd4bd206bb9ac728cee7a94a327f306
Michi <breitingermichael@gmail.com>
2026-02-06 18:58:33 +0100

    Unsafe tune

--- src/Settings.h
+++ src/Settings.h
@@ -3,10 +3,10 @@
 
 
 #define POWER_RANGE 100 // Max power to use
-#define LOOP_MS 4      // Time to let main loop sleep
+#define LOOP_MS 3      // Time to let main loop sleep
 
 #define MIN_SPEED 35    // Minimum speed to hold // 25
-#define TRAVEL_SPEED 76 // Avarage speed to hold // 60
+#define TRAVEL_SPEED 86 // Avarage speed to hold // 60
 #define MAX_SPEED 100   // Maximum speed to hold // 100
 
 const float update_time = LOOP_MS/1000; // 0.01 // seconds
